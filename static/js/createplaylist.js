188f45302c82385ca3408b2fd27b7922869a5b63
Georgina Garza <georginagarza@Georginas-MBP.attlocal.net>
2018-07-15 02:24:26 -0500

    playing sound; updating submit form; getting undefined for faved songs

--- static/js/createplaylist.js
+++ static/js/createplaylist.js
@@ -1,10 +1,38 @@
 var tempo = 0;
-var timing = tempo / 60;
+var bps = 60/tempo;
+var bpms = bps*1000;
+
+function updateTempo() {
+    var givenTempo = document.getElementById('tempo_input').value;
+    tempo = parseInt(givenTempo);
+    bps = 60/tempo;
+    bpms = bps*1000;
+    console.log("tempo updated: " + bps);
+}
 
 function playtempo() {
     console.log("Play Tempo");
+    setInterval(play_beat, bpms);
 }
 
 function submitInfo() {
-    console.log("Submit Info");
+    var givenTempo = document.getElementById('tempo_input').value;
+    tempo = parseInt(givenTempo);
+    var givenName = document.getElementById('playlist_name').value;
+    console.log("Submit Info: " + tempo + " " + givenName);
+    $.ajax({
+        url: 'https://api.spotify.com/v1/me/tracks',
+        headers: {
+          'Authorization': 'Bearer ' + authorization
+        },
+        success: function(response) {
+          var faved_songs = response;
+          console.log(response);
+        }
+    });
+}
+
+function play_beat() {
+    var sound = document.getElementById('linkAudio');
+    sound.play();
 }
