de15a2019e8536abfe12d46967e41e92e7037601
Georgina Garza <georginagarza@Georginas-MBP.attlocal.net>
2018-07-21 18:05:29 -0500

    goThroughSongs method completed

--- static/js/createplaylist.js
+++ static/js/createplaylist.js
@@ -39,41 +39,39 @@ function submitInfo() {
         }
     });
     var playlist_id = null;
-    $.ajax({
-        url: 'https://api.spotify.com/v1/users/' + client_id + '/playlists',
-        headers: {
-          'Authorization': 'Bearer ' + authorization,
-          'Content-Type': 'application/json'
-        },
-        data: "{\"name\":\"" + givenName + "\",\"public\":false}",
-        success: function(response) {
-            playlist_id = response.id
-        }
-    });
-    $.ajax({
-        url: 'https://api.spotify.com/v1/users/' + client_id + '/playlists/' + playlist_id + '/tracks',
-        headers: {
-          'Authorization': 'Bearer ' + authorization,
-          'Content-Type': 'application/json'
-        },
-        data: '{"uris":' + songs_to_add + '}',
-        success: function(response) {
-        }
-    });
-     $.ajax({url: "result", success: function(result) {
-     }});
+    // $.ajax({
+    //     url: 'https://api.spotify.com/v1/users/' + client_id + '/playlists',
+    //     headers: {
+    //       'Authorization': 'Bearer ' + authorization,
+    //       'Content-Type': 'application/json'
+    //     },
+    //     data: "{\"name\":\"" + givenName + "\",\"public\":false}",
+    //     success: function(response) {
+    //         playlist_id = response.id
+    //     }
+    // });
+    // $.ajax({
+    //     url: 'https://api.spotify.com/v1/users/' + client_id + '/playlists/' + playlist_id + '/tracks',
+    //     headers: {
+    //       'Authorization': 'Bearer ' + authorization,
+    //       'Content-Type': 'application/json'
+    //     },
+    //     data: '{"uris":' + songs_to_add + '}',
+    //     success: function(response) {
+    //     }
+    // });
+    //  $.ajax({url: "result", success: function(result) {
+    //  }});
 }
 
 function goThroughSongs(faved,tempoGiven) {
     console.log("I am in the other function");
-    //go through faved to get the spotify track ID
-    //with the track id look for tempo
-    //if within range, add it to result
     var result = [];
     for(var i = 0; i < faved.length; i++){
-        var track = faved[i];
-        var track_id = track.id;
-        var track_uri = track.uri;
+        var curr_track = faved[i];
+        var track_id = curr_track.track.id;
+        var track_uri = curr_track.track.uri;
+        // console.log("Track: " + JSON.stringify(curr_track) + " ID: " + JSON.stringify(track_id) + " URI: " + JSON.stringify(track_uri));
         var track_tempo = 0;
         $.ajax({
             url: 'https://api.spotify.com/v1/audio-features/' + track_id,
@@ -81,16 +79,13 @@ function goThroughSongs(faved,tempoGiven) {
               'Authorization': 'Bearer ' + authorization
             },
             success: function(response) {
-              var track_tempo_string = JSON.stringify(response.tempo);
-              track_tempo = response.tempo;
+              // console.log("TEMPO: " + response.tempo);
+              track_tempo = parseInt(response.tempo);
+               if((tempoGiven - 5 <= track_tempo) && (track_tempo <= tempoGiven + 5)) {
+                    result.push(track_uri);
+                }
             }
         });
-        if((tempoGiven - 5 <= track_tempo) || (track_tempo <= tempoGiven + 5)) {
-            result.push(track_uri);
-            if(result.length >= 100){
-                break;
-            }
-        }
     }
     return result;
 }
